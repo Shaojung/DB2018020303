cb621d7f4877abefbc6ab6370394fe9eb0ea5255
Shaojung <shaojung@gmail.com>
2018-02-03 11:36:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/db2018020303/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.student.db2018020303;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView wv = (WebView) findViewById(R.id.webView);
+        wv.setWebChromeClient(new WebChromeClient());
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl("http://www.pcschool.com.tw");
+    }
+}
